385502e33d6817d79962dcbb0b1a20e4ff25cc94
sanirufee <Muhammedasanya2020@gmail.com>
2023-04-02 15:33:55 -0700

    Good

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,43 @@
 #!/usr/bin/python3
-Square = __import__('4-square').Square
+"""The square module continue"""
 
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+class Square:
+    """The class to create square."""
 
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
+    def __init__(self, size=0):
+        """set size to private instance variable
+        Args:
+            size (int): the size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """get the area of the square
+        Args:
+            None
+        Returns:
+            Area of the square(int)
+        """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """getter that get size variable"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set thesize to the value
+        Args:
+            value: the value to reset
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
