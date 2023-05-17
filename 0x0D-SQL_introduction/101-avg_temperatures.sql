d8203dbb8b871df31c0b97dd218fdfea4af2b955
sanirufee <Muhammedasanya2020@gmail.com>
2023-05-16 17:46:03 -0700

    sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- Displlay the average temperature of city in F
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;-- Displlay the average temperature of city in F
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
